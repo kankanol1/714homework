85fe0e15f2a2c9f2318adc90497cd2c52f705675
kankan <1317894008@qq.com>
2018-07-15 15:41:48 +0800

    Initial commit

---
+++ 0714/js/data.js
@@ -0,0 +1,27 @@
+var dataInfo = [
+    {
+        "img1":"img/01.png",
+        "img2":"img/01a.png",
+        "info":"信息管家"
+    },
+    {
+        "img1":"img/01.png",
+        "img2":"img/01a.png",
+        "info":"无线音乐专区"
+    },
+    {
+        "img1":"img/01.png",
+        "img2":"img/01a.png",
+        "info":"手机证券"
+    },
+    {
+        "img1":"img/01.png",
+        "img2":"img/01a.png",
+        "info":"飞信"
+    },
+    {
+        "img1":"img/01.png",
+        "img2":"img/01a.png",
+        "info":"短信回执"
+    }
+];
